6ea214dd7dc27f05d422cb5502bd5c78c9f064c7
TylerSpaulding <TylerSpaulding95@gmail.com>
2019-11-11 22:19:17 -0500

    Project router crud operations complete

--- projects/projectRouter.js
+++ projects/projectRouter.js
@@ -17,7 +17,59 @@ router.get('/', async (req, res) => {
         });
     };
 });
-//====================//
+//======================//
 
+/* POST: /api/projects */
+router.post('/', async (req, res) => {
+    const action = req.params.body;
+    try {
+        const projects = Project.insert(action)
+        res.status(200).json({
+            message: `${projects} project added!`
+        }) 
+    }   
+    catch(err) {
+        console.log(err);
+        res.status(500).json({
+            message: 'Error adding new project!'
+        });
+    };
+});
+//=========================//
 
+/* PUT: /api/projects/:id */
+router.put('/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+        const update = Project.update(id)
+        res.status(200).json({
+            message: `Project with ID: ${id} updated! to ${update}`
+        })
+    }
+    catch(err) {
+        console.log(err);
+        res.status(500).json({
+            message: `Error updating projet with ID: ${id}!`
+        });
+    };
+});
+//============================//
+
+/* DELETE: /api/projects/:id */
+router.delete('/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+        const deleted = Project.delete(id)
+        res.status(200).json({
+            message: `Project with ID: ${id} deleted!`
+        })
+    }
+    catch(err) {
+        console.log(err);
+        res.status(500).json({
+            message: `Error deleting project with ID: ${id}`
+        });
+    };
+});
+//===========================//
 
